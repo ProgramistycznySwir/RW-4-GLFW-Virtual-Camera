674c228d794fb7ab87ebcf449d5993e056ba3e7e
Daniel Pietrzeniuk <ProgramistycznySwir@gmail.com>
2021-11-24 21:41:50 +0100

    Finished

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <glad/glad.h>
+﻿#include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
@@ -6,13 +6,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-// #include <algorithm> // For clamp
 
-static struct
+
+static const struct
 {
     float x, y, z;
     float r, g, b;
+
 } vertices[48] =
 {
     {1.0f,-1.0f,0.0f,1.0f,1.0f,1.0f},
@@ -66,7 +66,6 @@ static struct
 };
 
 #pragma region >>> Shaders <<<
-
 static const char* vertex_shader_text =
 "#version 110\n"
 "uniform mat4 MVP;\n"
@@ -86,13 +85,13 @@ static const char* fragment_shader_text =
 "{\n"
 "    gl_FragColor = vec4(color, 1.0);\n"
 "}\n";
-
 #pragma endregion
 
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
+
 
 float Clamp(float value, float min, float max)
 {
@@ -102,110 +101,107 @@ float Clamp(float value, float min, float max)
         return min;
     return value;
 }
-
-struct Camera {
+struct Player {
+    vec3 Position;
     float Yaw;
     float Pitch;
-    float move_Z;
-    float move_X;
-    float move_fov;
-    vec3 x_d;
-    vec3 y_d;
-    vec3 z_d;
-    vec3 Pos;
-
-} camera =
+    float X_Change;
+    float Z_Change;
+    float fov_Change;
+    vec3 VecX;
+    vec3 VecY;
+    vec3 VecZ;
+} player =
 {
-    0, 0, 0, 0, 0,
+    {0, 0, 5},
+    0.25f, 0, 0, 0, 0,
     {1, 0, 0},
     {0, 1, 0},
     {0, 0, 1},
-    {0, 0, 2},
 };
 
+const float MovementSpeed = 0.06f;
+const float FovChangeSpeed = 0.025f;
 static void Key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
-    
-    if (key == GLFW_KEY_KP_SUBTRACT || key == GLFW_KEY_KP_ADD) { //zwiększanie i zmniejszanie kątów widzenia
-        if (action == GLFW_PRESS)
-            camera.move_fov = key == GLFW_KEY_KP_ADD ? -0.05f : 0.05f;
-        if (action == GLFW_RELEASE)
-            camera.move_fov = 0;
-    }
 
-    if (key == GLFW_KEY_A || key == GLFW_KEY_D) { //w lewo, w prawo
-        if (action == GLFW_PRESS)
-            camera.move_X = key == GLFW_KEY_A ? -0.05f : 0.05f;
-        if (action == GLFW_RELEASE)
-            camera.move_X = 0;
+    if (action == GLFW_PRESS) {
+        player.X_Change -= (key == GLFW_KEY_A) * MovementSpeed;
+        player.X_Change += (key == GLFW_KEY_D) * MovementSpeed;
     }
+    else if (action == GLFW_RELEASE && ((key == GLFW_KEY_A) || (key == GLFW_KEY_D)))
+        player.X_Change = 0;
 
-    if (key == GLFW_KEY_S || key == GLFW_KEY_W) { //s do tyłu, w do przodu
-        if (action == GLFW_PRESS)
-            camera.move_Z = key == GLFW_KEY_W ? -0.05f : 0.05f;
-        if (action == GLFW_RELEASE)
-            camera.move_Z = 0;
+    if (action == GLFW_PRESS) {
+        player.Z_Change -= (key == GLFW_KEY_W) * MovementSpeed;
+        player.Z_Change += (key == GLFW_KEY_S) * MovementSpeed;
     }
+    else if (action == GLFW_RELEASE && ((key == GLFW_KEY_W) || (key == GLFW_KEY_S)))
+        player.Z_Change = 0;
+
+    if (action == GLFW_PRESS) {
+        player.fov_Change -= (key == GLFW_KEY_KP_ADD) * FovChangeSpeed;
+        player.fov_Change += (key == GLFW_KEY_KP_SUBTRACT) * FovChangeSpeed;
+    }
+    else if (action == GLFW_RELEASE)
+        player.fov_Change = 0;
 }
 
+const float MouseSensitivity = 0.0025f;
 static void Mouse_callback(GLFWwindow* window, double x_pos, double y_pos)
 {
-    static double x_prev_pos = 0;
-    static double y_prev_pos = 0;
+    static float x_prev = 0;
+    static float y_prev = 0;
 
-    double x_dif = x_pos - x_prev_pos;
-    double y_dif = y_pos - y_prev_pos;
+    if (fabs(x_pos - x_prev) > 0)
+        player.Yaw -= (x_pos - x_prev) * MouseSensitivity;
+    if (fabs(y_pos - y_prev) > 0)
+        player.Pitch -= (y_pos - y_prev) * MouseSensitivity;
+    player.Pitch = Clamp(player.Pitch, -1.0f, 1.0f);
 
-    x_prev_pos = x_pos;
-    y_prev_pos = y_pos;
-
-    if (fabs(x_dif) > 0)
-        camera.Yaw += x_dif * 0.005;
-    if (fabs(y_dif) > 0)
-        camera.Pitch -= y_dif * 0.005;
+    x_prev = x_pos;
+    y_prev = y_pos;
 }
 const float fov_min = 0.174f; // 10 stopni
 const float fov_max = 2.094f; // 120 stopni
 const float Camera_NearClippingPlane = .1f;
 const float Camera_FarClippingPlane = 100.f;
 
-void LookAt(mat4x4 v, struct Camera* camera) {
-    mat4x4 world_cam, translate;
+void LookAt(mat4x4 v, struct Player* player) {
+    mat4x4 world_cam;
     mat4x4_identity(world_cam);
+    mat4x4_rotate_Y(world_cam, world_cam, (float)player->Yaw);
+    mat4x4_rotate_X(world_cam, world_cam, (float)player->Pitch);
 
-    mat4x4_rotate_Y(world_cam, world_cam, (float)camera->Yaw);
-    mat4x4_rotate_X(world_cam, world_cam, (float)camera->Pitch);
-    mat4x4_translate(translate, camera->Pos[0], camera->Pos[1], camera->Pos[2]);
+    mat4x4 translate;
+    mat4x4_translate(translate, player->Position[0], player->Position[1], player->Position[2]);
 
     mat4x4_mul(world_cam, translate, world_cam);
     mat4x4_invert(v, world_cam);
 
-    camera->x_d[0] = world_cam[0][0];
-    camera->x_d[1] = world_cam[0][1];
-    camera->x_d[2] = world_cam[0][2];
-    camera->y_d[0] = world_cam[1][0];
-    camera->y_d[1] = world_cam[1][1];
-    camera->y_d[2] = world_cam[1][2];
-    camera->z_d[0] = world_cam[2][0];
-    camera->z_d[1] = world_cam[2][1];
-    camera->z_d[2] = world_cam[2][2];
+    /// Apply world cam
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+        player->VecX[i] = world_cam[0][i];
+        player->VecY[i] = world_cam[1][i];
+        player->VecZ[i] = world_cam[2][i];
+    }
 }
-
 
 
 int main(void)
 {
-    /// Scale shape:
-    // float scale = 0.25f;
+    // Scale shape:
+    // float scale = 12.25f;
     // for(int i = 0; i < 48; i++)
     // {
     //     vertices[i].x *= scale;
     //     vertices[i].y *= scale;
     //     vertices[i].z *= scale;
     // }
-
 
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
@@ -219,12 +215,18 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(640, 480, "GLFW Virtual Camera", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Example", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPos(window, 0, 0);
+
+    glfwSetKeyCallback(window, Key_callback);
+    glfwSetCursorPosCallback(window, Mouse_callback);
 
     glfwMakeContextCurrent(window);
     // gladLoadGL(glfwGetProcAddress);
@@ -256,89 +258,49 @@ int main(void)
 
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
-            sizeof(vertices[0]), (void*) 0);
+        sizeof(vertices[0]), (void*)0);
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-            sizeof(vertices[0]), (void*) (sizeof(float) * 3));
+        sizeof(vertices[0]), (void*)(sizeof(float) * 3));
     // glEnable(GL_DEPTH_TEST);
 
     glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //glDepthFunc(GL_LESS);
 
-    /// Player position and rotation:
     float fov = 1.0f;
-    // vec3 player_pos = { 0.f, 0.f, 2.f};
-     // X is for Yaw, Y is for Pitch
-    /// Register for actions related to player:
-    // glfwSetKeyCallback(window, GatherWASD_KeyCallback);
 
-    /// Setup input:
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwSetCursorPos(window, 0, 0);
-
-    glfwSetKeyCallback(window, Key_callback);
-    glfwSetCursorPosCallback(window, Mouse_callback);
-
-    time_t lastFrameTime = time(NULL);
     while (!glfwWindowShouldClose(window))
     {
-        /// Calculating deltaTime:
-        time_t currentFrameTime = time(NULL);
-        float deltaTime = currentFrameTime - lastFrameTime;
-        lastFrameTime = currentFrameTime;
-
-        /// Applying player input:
-        float playerSpeed = 0.5f;
-        float mouseSensitivity = 0.2f;
-        // vec3 player_forward = {1 * sinf(player_rot.yaw), 1 * cosf(player_rot.yaw)};
-        /// Player movement...
-        vec3 temp;
-        // vec3_scale(temp, { 0.f, 0.f, 1.f }, isKeyPressed.w * playerSpeed * deltaTime);
-        // vec3_add(player_pos, player_pos, temp);
-        // vec3_add(player_pos, player_pos, { 0.f, 0.f, isKeyPressed.w * playerSpeed * deltaTime });
-        // vec3_add(player_pos, player_pos, { 0.f, 0.f, - isKeyPressed.s * playerSpeed * deltaTime });
-        // vec3_add(player_pos, player_pos, { - isKeyPressed.a * playerSpeed * deltaTime , 0.f, 0.f });
-        // vec3_add(player_pos, player_pos, { isKeyPressed.s * playerSpeed * deltaTime , 0.f, 0.f });
-
-        fov += (float)camera.move_fov;
-        fov = Clamp(fov, fov_min, fov_max);
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         float ratio;
         int width, height;
-        mat4x4 m, v, p;
+        mat4x4 m, v, p, mvp;
 
         glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
+        ratio = width / (float)height;
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         mat4x4_identity(p);
+
+        fov += (float)player.fov_Change;
+        fov = Clamp(fov, fov_min, fov_max);
+
         mat4x4_perspective(m, fov, ratio, Camera_NearClippingPlane, Camera_FarClippingPlane);
 
         vec3 direction;
-        vec3_scale(direction, camera.x_d, (float)camera.move_X);
-        vec3_add(camera.Pos, camera.Pos, direction);
-        vec3_scale(direction, camera.z_d, (float)camera.move_Z);
-        vec3_add(camera.Pos, camera.Pos, direction);
-        /// Transforms:
-        // mat4x4_translate(m,  (float) glfwGetTime() * 0.05f, 0.f, 0.f);
-        // mat4x4_rotate_X(m, m, (float) glfwGetTime());
-        // mat4x4_rotate_Y(m, m, (float) glfwGetTime() / 2.0f);
+        vec3_scale(direction, player.VecX, (float)player.X_Change);
+        vec3_add(player.Position, player.Position, direction);
+        vec3_scale(direction, player.VecZ, (float)player.Z_Change);
+        vec3_add(player.Position, player.Position, direction);
 
-        // mat4x4_perspective(m, 0.5, ratio, .1f, 100.f);
-        LookAt(v, &camera);
+        LookAt(v, &player);
 
-        mat4x4 mvp;
         mat4x4_mul(mvp, m, v);
-        mat4x4_mul(mvp, mvp, m);
+        mat4x4_mul(mvp, mvp, p);
 
-        /// Rendering:
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
         glDrawArrays(GL_TRIANGLES, 0, 48);
 
         glfwSwapBuffers(window);
